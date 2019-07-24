5f62c43dacbd8707988b456737163a77412d7264
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-24 13:04:47 -0700

    Make anagrams problem

--- StringManipulation/MakingAnagrams.js
+++ StringManipulation/MakingAnagrams.js
@@ -1,15 +1,24 @@
 /*
-Alice is taking a cryptography class and finding anagrams to be very useful. We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+Alice is taking a cryptography class and finding anagrams to be very useful. We consider two strings to be 
+anagrams of each other if the first string's letters can be rearranged to form the second string. In other 
+words, both strings must contain the same exact letters in the same exact frequency For example, bacdc and 
+dcbac are anagrams, but bacdc and dcbad are not.
 
-Alice decides on an encryption scheme involving two large strings where encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Can you help her find this number?
+Alice decides on an encryption scheme involving two large strings where encryption is dependent on the 
+minimum number of character deletions required to make the two strings anagrams. Can you help her find 
+this number?
 
-Given two strings,  and , that may or may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+Given two strings,  and , that may or may not be of the same length, determine the minimum number of
+ character deletions required to make  and  anagrams. Any characters can be deleted from either of the 
+ strings.
 
-For example, if a=cde and b=dcf, we can delete e from string a and f from string b so that both remaining strings are cd and dc which are anagrams.
+For example, if a=cde and b=dcf, we can delete e from string a and f from string b so that both remaining 
+strings are cd and dc which are anagrams.
 
 Function Description
 
-Complete the makeAnagram function in the editor below. It must return an integer representing the minimum total characters that must be deleted to make the strings anagrams.
+Complete the makeAnagram function in the editor below. It must return an integer representing the minimum
+ total characters that must be deleted to make the strings anagrams.
 
 makeAnagram has the following parameter(s):
 
@@ -25,7 +34,8 @@ Constraints
 The strings a and b consist of lowercase English alphabetic letters ascii[a-z].
 Output Format
 
-Print a single integer denoting the number of characters you must delete to make the two strings anagrams of each other.
+Print a single integer denoting the number of characters you must delete to make the two strings anagrams
+ of each other.
 
 Sample Input
 
