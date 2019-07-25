3580d5f3e6381d0f11c46e84788f9d20ec392a7c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-24 19:53:26 -0700

    MVP hurdle problem

--- Algorithm/TheHurdleChallenge.js
+++ Algorithm/TheHurdleChallenge.js
@@ -1,3 +1,16 @@
 /*
 Find max different between k and all height available.
 */
+function hurdleRace(k, height) {
+    
+    var maxHeight = 0;
+    for (var i = 0; i < height.length; i++) {
+        if (height[i] > maxHeight) {
+            maxHeight = height[i];
+        }
+    }
+    if (maxHeight > k) {
+        return maxHeight - k;
+    }
+    return 0;
+}
