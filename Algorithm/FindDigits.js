53b6c2f8e342bd3e024aec908ee3f8f29026454e
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-25 01:08:54 -0700

    MVP digit number

--- Algorithm/FindDigits.js
+++ Algorithm/FindDigits.js
@@ -1,3 +1,14 @@
 /*
 Given an internet n find out how many of it's digit is n divisible by.
 */
+function findDigits(n) {
+    var num = n.toString().split('');
+    var divisor = 0;
+    for (var i = 0; i < num.length; i++){
+        if (n % num[i] === 0) {
+            divisor++;
+        }
+    }
+    return divisor;
+
+}
