fa7d0dd83cbe21c63954bca85c7a967bb5b3780f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-25 01:12:07 -0700

    MVP factorial problem

--- Algorithm/ExtraLongFactorial.js
+++ Algorithm/ExtraLongFactorial.js
@@ -1,3 +1,12 @@
 /*
 Calculate factorial of n
 */
+function extraLongFactorials(n) {
+    var factorial = 1;
+    for (var i = n; i >= 1; i--) {
+        factorial *= i;
+    }
+    console.log(factorial);
+    return factorial;
+
+}
