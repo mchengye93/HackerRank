38c6d3b5b50f689c7d791efcb5718d8764fbf6f3
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-24 20:00:21 -0700

    MVP PDF problem

--- Algorithm/DesignerPdfViewer.js
+++ Algorithm/DesignerPdfViewer.js
@@ -1,3 +1,16 @@
 /*
 Given height of each letter represent by h. Find the area taken by the word.
 */
+function designerPdfViewer(h, word) {
+    console.log(h);
+    var arr = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var max = -1;
+
+    for (var i = 0; i < word.length; i++){
+        var letter = word[i];
+        if (h[arr.indexOf(letter)] > max) {
+            max = h[arr.indexOf(letter)];
+        }
+    }
+    return max * word.length;
+}
