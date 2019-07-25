e7bab88da42df35600e4cb5e35afa02be91445bc
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-24 19:35:03 -0700

    Pick number prompt

--- Algorithm/PickingNumber.js
+++ Algorithm/PickingNumber.js
@@ -1,24 +1,30 @@
 /*
-Given an array of integers, find and print the maximum number of integers you can select from the array such that the absolute difference between any two of the chosen integers is less than or equal to . For example, if your array is , you can create two subarrays meeting the criterion:  and . The maximum length subarray has  elements.
+Given an array of integers, find and print the maximum number of integers you can select from the array such
+ that the absolute difference between any two of the chosen integers is less than or equal to 1 . For example, 
+ if your array is a=[1,1,2,2,4,4,5,5,5], you can create two subarrays meeting the criterion: [1,1,2,2] and[4,4,5,5,5] . 
+ The maximum length subarray has 5 elements.
 
 Function Description
 
-Complete the pickingNumbers function in the editor below. It should return an integer that represents the length of the longest array that can be created.
+Complete the pickingNumbers function in the editor below. It should return an integer that represents the length
+ of the longest array that can be created.
 
 pickingNumbers has the following parameter(s):
 
 a: an array of integers
+
 Input Format
 
-The first line contains a single integer , the size of the array . 
-The second line contains  space-separated integers .
+The first line contains a single integer , the size of the array a. 
+The second line contains n space-separated integers a[i] .
 
 Constraints
-
-The answer will be .
+2 <= n <= 100
+The answer will be >= 2 .
 Output Format
 
-A single integer denoting the maximum number of integers you can choose from the array such that the absolute difference between any two of the chosen integers is .
+A single integer denoting the maximum number of integers you can choose from the array such that the absolute
+ difference between any two of the chosen integers is <= 1 .
 
 Sample Input 0
 
