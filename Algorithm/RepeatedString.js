9d98aae06b90d76f065489d10622eae965de96c5
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-25 15:10:26 -0700

    Somewhat working solution

--- Algorithm/RepeatedString.js
+++ Algorithm/RepeatedString.js
@@ -6,3 +6,22 @@ Given an integer, n, find and print the number of letter a's in the first n lett
 For example, if the string s='abcac' and n=10, the substring we consider is  abcacabcac, the first 10 characters of
 her infinite string.  There are  occurrences of a in the substring.
 */
+function repeatedString(s, n) {
+    var repeatString = '';
+    var numTimesRepeat = Math.ceil(n/s.length);
+    console.log(numTimesRepeat);
+    for (var i = 0; i < numTimesRepeat; i++){
+        repeatString += s;
+        
+    }
+    console.log(repeatString);
+    var aCount = 0;
+
+    for (var i = 0; i < n; i++){
+        if (repeatString[i] === 'a') {
+            aCount++;
+        }
+    }
+    return aCount;
+ 
+}
