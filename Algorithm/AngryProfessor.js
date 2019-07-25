fdde47f9f7130cc1ea0282b3cfa664d5a17113a9
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-24 20:09:16 -0700

    Angry professor problem

---
+++ Algorithm/AngryProfessor.js
@@ -0,0 +1,4 @@
+/*
+Given k, min number of student for class to go on and a an array of student time arrival <=0 being on time
+ and > 0 late. If min number of student on time is not met then class is cancelled.
+*/
