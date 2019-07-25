5387963a1885dfafaed8601bf306caa670971e31
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-24 19:33:02 -0700

    Picking number problem

---
+++ Algorithm/PickingNumber.js
@@ -0,0 +1,30 @@
+/*
+Given an array of integers, find and print the maximum number of integers you can select from the array such that the absolute difference between any two of the chosen integers is less than or equal to . For example, if your array is , you can create two subarrays meeting the criterion:  and . The maximum length subarray has  elements.
+
+Function Description
+
+Complete the pickingNumbers function in the editor below. It should return an integer that represents the length of the longest array that can be created.
+
+pickingNumbers has the following parameter(s):
+
+a: an array of integers
+Input Format
+
+The first line contains a single integer , the size of the array . 
+The second line contains  space-separated integers .
+
+Constraints
+
+The answer will be .
+Output Format
+
+A single integer denoting the maximum number of integers you can choose from the array such that the absolute difference between any two of the chosen integers is .
+
+Sample Input 0
+
+6
+4 6 5 3 3 1
+Sample Output 0
+
+3
+*/
