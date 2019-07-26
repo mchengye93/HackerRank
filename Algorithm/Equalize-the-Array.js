235879e353ff56486f686396cb8c237f4540342f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-26 16:43:15 -0700

    Equalize array problem

---
+++ Algorithm/Equalize-the-Array.js
@@ -0,0 +1,18 @@
+/*
+Given an array of integers. Find the minimum number of deletes to equalize array.
+
+Karl has an array of integers. He wants to reduce the array until all remaining elements are equal. 
+Determine the minimum number of elements to delete to reach his goal.
+
+For example, if his array is arr = [1,2,2,3], we see that he can delete the 2 elements 1 and 3 leaving arr = [2,2]. 
+He could also delete both twos and either the 1 or the 3, but that would take  deletions. 
+The minimum number of deletions is 2 .
+
+Sample Input
+
+5
+3 3 2 1 3
+Sample Output
+
+2   
+*/
