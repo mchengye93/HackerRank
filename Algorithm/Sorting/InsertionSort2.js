ce35d0006ace7196821adf24601c0a3a8617a7b4
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-26 20:14:00 -0700

    MVP Insertion sort part 2

--- Algorithm/Sorting/InsertionSort2.js
+++ Algorithm/Sorting/InsertionSort2.js
@@ -1,3 +1,23 @@
 /*
 Insertion sort Part 2
 */
+function insertionSort2(n, arr) {
+    for (var i = 1; i < arr.length; i++) {
+        var current = arr[i];
+        for (var x = 0; x < i; x++) {
+            var prev = arr[x];
+
+            if (prev > current) {
+                //swap position
+                var temp = current;
+                arr[i] = prev;
+                arr[x] = temp;
+                current = prev;
+
+            }
+            
+        }
+        console.log(arr.join(' '));    
+    }
+
+}
