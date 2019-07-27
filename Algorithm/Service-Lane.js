d9b80366fa1d4189584db1d7b02f2b5431b96447
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-26 17:24:35 -0700

    MVP service lane

--- Algorithm/Service-Lane.js
+++ Algorithm/Service-Lane.js
@@ -7,3 +7,21 @@ cases =[ [0 ,3],[4,6], [6,7],[3,5], [0,7] ]
 Sample Output
 [1,2 ,3 , 2,1]
 */
+function serviceLane(n, width, cases) {
+   
+    var minWidth = [];
+    for (var i = 0; i < cases.length; i++) {
+        var start = cases[i][0];
+        var end = cases[i][1];
+        var min = width[start];
+        for (var x = start; x <= end; x++){
+            if (width[x] < min) {
+                min = width[x];
+            }
+        }
+        minWidth.push(min);
+
+    }
+    return minWidth;
+    
+}
