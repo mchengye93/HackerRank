38d087337ddceb7cf53b5ca7c83c6e1923f828ca
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-26 17:24:17 -0700

    Service lane problem

---
+++ Algorithm/Service-Lane.js
@@ -0,0 +1,9 @@
+/*
+Given a list of integers showing width at particular indexes. Find minimum width
+at given start and end indexes.
+Sample Input
+width = [2 ,3 ,1 ,2, 3, 2 ,3, 3]
+cases =[ [0 ,3],[4,6], [6,7],[3,5], [0,7] ]
+Sample Output
+[1,2 ,3 , 2,1]
+*/
