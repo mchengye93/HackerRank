533999e92eb0513d10c29fd21eb2bdbeb8615e9f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-26 19:51:03 -0700

    Somewhat working insertion sort 1

--- Algorithm/Sorting/InsertionSort1.js
+++ Algorithm/Sorting/InsertionSort1.js
@@ -1,3 +1,15 @@
 /*
 Insertion Sort 1 Problem
 */
+function insertionSort1(n, arr) {
+    var temp = arr[arr.length - 1];
+    for (var i = arr.length - 1; i > 0; i--){
+        if (arr[i-1] >= temp) {
+            arr[i] = arr[i-1];
+        } else {
+            arr[i] = temp;
+        }
+        console.log(arr.toString().split(',').join(' '));
+    }
+   
+}
