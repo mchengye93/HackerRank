a9d4a96b301cffd41e74dbe408b7167542d95060
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-26 18:05:07 -0700

    MVP chocolate problem

--- Algorithm/ChocolateFeast.js
+++ Algorithm/ChocolateFeast.js
@@ -2,3 +2,21 @@
 There is a chocolate promotion. Each bar cost c and you can exchange one free chocolate bar for every m wraps.
 Given a budget of n. Find total number of chocolate bar you can eat before you run out of budget.
 */
+//n - budget
+// c - cost of chocolate bar
+// m - number of wrapper to exchange into free bar
+function chocolateFeast(n, c, m) {
+    //total number of chocolate bar bought & num of wrapper
+    var numOfBar = Math.floor(n / c);
+    var numOfWrapper = Math.floor(n / c);
+
+    while (numOfWrapper >= m) {
+        var leftOverWraps = numOfWrapper % m;
+        var barExchanged = Math.floor(numOfWrapper / m);
+        numOfBar += barExchanged;
+        //remaining number of wrapper
+        numOfWrapper = leftOverWraps + barExchanged;
+    }
+    return numOfBar;
+
+}
