395bd1f8079022db58d90c61995be28f8b5f36f7
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-26 17:02:32 -0700

    MVP beautiful triple problem

--- Algorithm/BeautifulTriplets.js
+++ Algorithm/BeautifulTriplets.js
@@ -4,3 +4,15 @@ Given an array of integer in ascending order find the 3 integers that would sati
  a[j] - a[i] = d;
  a[k] - a[j] = d;
 */
+function beautifulTriplets(d, arr) {
+    var triplets = [];
+    for (var i = 0; i < arr.length - 2; i++){
+        var num = arr[i];
+        if (arr.indexOf(num + d) !== -1 && arr.indexOf(num + 2*d) !== -1) {
+            triplets.push([num, num + d, num + 2 * d]);
+        }
+
+    }
+    return triplets.length;
+
+}
